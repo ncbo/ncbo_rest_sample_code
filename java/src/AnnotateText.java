1a09b095d30b51c5a4c4d04bc31ab2fb31610693
Paul R Alexander <palexander@stanford.edu>
2013-04-26 14:54:13 -0700

    Java annotate text example

---
+++ java/src/AnnotateText.java
@@ -0,0 +1,91 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URLEncoder;
+
+public class AnnotateText {
+
+    static final String REST_URL = "http://stagedata.bioontology.org";
+    static final String API_KEY = "";
+    static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws Exception {
+        String textToAnnotate = URLEncoder.encode("Melanoma is a malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye.", "ISO-8859-1");
+        
+        // Get just annotations
+        JsonNode annotations = jsonToNode(get(REST_URL + "/annotator?text=" + textToAnnotate));
+        printAnnotations(annotations);
+        
+        // Annotations with hierarchy
+        annotations = jsonToNode(get(REST_URL + "/annotator?max_level=3&text=" + textToAnnotate));
+        printAnnotations(annotations);
+    }
+    
+    private static void printAnnotations(JsonNode annotations) {
+        for (JsonNode annotation : annotations) {
+            // Get the details for the class that was found in the annotation and print
+            JsonNode classDetails = jsonToNode(get(annotation.get("annotatedClass").get("links").get("self").asText()));
+            System.out.println("Class details");
+            System.out.println("\tid: " + classDetails.get("@id").asText());
+            System.out.println("\tprefLabel: " + classDetails.get("prefLabel").asText());
+            System.out.println("\tontology: " + classDetails.get("links").get("ontology").asText());
+            System.out.println("\n");
+
+            JsonNode hierarchy = annotation.get("hierarchy");
+            // If we have hierarchy annotations, print the related class information as well
+            if (hierarchy.isArray() && hierarchy.elements().hasNext()) {
+                System.out.println("\tHierarchy annotations");
+                for (JsonNode hierarchyAnnotation : hierarchy) {
+                    classDetails = jsonToNode(get(hierarchyAnnotation.get("annotatedClass").get("links").get("self").asText()));
+                    System.out.println("\t\tClass details");
+                    System.out.println("\t\t\tid: " + classDetails.get("@id").asText());
+                    System.out.println("\t\t\tprefLabel: " + classDetails.get("prefLabel").asText());
+                    System.out.println("\t\t\tontology: " + classDetails.get("links").get("ontology").asText());
+                }
+            }
+        }
+    }
+
+    private static JsonNode jsonToNode(String json) {
+        JsonNode root = null;
+        try {
+            root = mapper.readTree(json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return root;
+    }
+
+    private static String get(String urlToGet) {
+        URL url;
+        HttpURLConnection conn;
+        BufferedReader rd;
+        String line;
+        String result = "";
+        try {
+            url = new URL(urlToGet);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Authorization", "apikey token=" + API_KEY);
+            conn.setRequestProperty("Accept", "application/json");
+            rd = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            while ((line = rd.readLine()) != null) {
+                result += line;
+            }
+            rd.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
