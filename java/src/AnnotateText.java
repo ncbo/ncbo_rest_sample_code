5bb8b7fdfab04b54551a0c3e92a77d285b5bff1c
Paul R Alexander <palexander@stanford.edu>
2014-03-05 12:10:23 -0800

    Update Java annotator example with POST and labels

--- java/src/AnnotateText.java
+++ java/src/AnnotateText.java
@@ -1,13 +1,13 @@
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URLEncoder;
 
 public class AnnotateText {
@@ -17,15 +17,31 @@ public class AnnotateText {
     static final ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) throws Exception {
+        String urlParameters;
+        JsonNode annotations;
         String textToAnnotate = URLEncoder.encode("Melanoma is a malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye.", "ISO-8859-1");
 
         // Get just annotations
-        JsonNode annotations = jsonToNode(get(REST_URL + "/annotator?text=" + textToAnnotate));
+        urlParameters = "text=" + textToAnnotate;
+        annotations = jsonToNode(get(REST_URL + "/annotator?" + urlParameters));
         printAnnotations(annotations);
 
         // Annotations with hierarchy
-        annotations = jsonToNode(get(REST_URL + "/annotator?max_level=3&text=" + textToAnnotate));
+        urlParameters = "max_level=3&text=" + textToAnnotate;
+        annotations = jsonToNode(get(REST_URL + "/annotator?" + urlParameters));
         printAnnotations(annotations);
+
+        // Annotations using POST (necessary for long text)
+        urlParameters = "text=" + textToAnnotate;
+        annotations = jsonToNode(post(REST_URL + "/annotator", urlParameters));
+        printAnnotations(annotations);
+
+        // Get labels, synonyms, and definitions with returned annotations
+        urlParameters = "include=prefLabel,synonym,definition&text=" + textToAnnotate;
+        annotations = jsonToNode(get(REST_URL + "/annotator?" + urlParameters));
+        for (JsonNode annotation : annotations) {
+            System.out.println(annotation.get("annotatedClass").get("prefLabel").asText());
+        }
     }
 
     private static void printAnnotations(JsonNode annotations) {
@@ -88,4 +104,42 @@ public class AnnotateText {
         }
         return result;
     }
+
+    private static String post(String urlToGet, String urlParameters) {
+        URL url;
+        HttpURLConnection conn;
+
+        String line;
+        String result = "";
+        try {
+            url = new URL(urlToGet);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setInstanceFollowRedirects(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", "apikey token=" + API_KEY);
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("charset", "utf-8");
+            conn.setUseCaches(false);
+
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.flush();
+            wr.close();
+            conn.disconnect();
+
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            while ((line = rd.readLine()) != null) {
+                result += line;
+            }
+            rd.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
 }
